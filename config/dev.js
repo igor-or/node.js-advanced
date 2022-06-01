ca4f4744d52dde171f62c574cab3fb371d4ad074
Ihor Oryshchenko <igor89.or@gmail.com>
2022-06-02 01:02:19 +0300

    changed dev credentials

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    googleClientID:
+        '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+    googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+    mongoURI:
+        'mongodb://igor:sarnovichu@cluster0-shard-00-00.jw29a.mongodb.net:27017,cluster0-shard-00-01.jw29a.mongodb.net:27017,cluster0-shard-00-02.jw29a.mongodb.net:27017/node-advanced?ssl=true&replicaSet=atlas-xjkqx5-shard-0&authSource=admin&retryWrites=true&w=majority',
+    cookieKey: '123123123',
 };
